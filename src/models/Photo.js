3682ec2c27a94395155b7f21c76dee79e2797b7b
AlyssonSilvaOpus <108431099+AlyssonSilvaOpus@users.noreply.github.com>
2023-01-18 00:24:25 -0300

    models

---
+++ src/models/Photo.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const fotoSchema = new Schema(
+    {
+        image: String,
+        title: String,
+        likes: Array,
+        comments: Array,
+        userId: mongoose.ObjectId,
+        userName: String,
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const Photo = mongoose.model('User', fotoSchema)
+
+module.exports = Photo
